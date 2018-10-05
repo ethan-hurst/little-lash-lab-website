d24f359309d78c2b8304116e95209fe9da10988d
flamebeard <ethan@hursty.co.nz>
2018-10-05 13:04:33 +1300

    changed emails

--- src/components/Contact-Icons.jsx
+++ src/components/Contact-Icons.jsx
@@ -7,7 +7,8 @@ export default class ContactIcons extends React.Component {
             <div className="SocialIcons">
                 <SocialIcon url="https://www.facebook.com/pg/thelittlelashlab" />
                 <SocialIcon url="https://www.instagram.com/thelittlelashlab/" />
-                <SocialIcon url="mailto:berenice@littlelashlab.com" />
+                <SocialIcon url="mailto:thelittlelashlab@gmail.com" />
+                <SocialIcon url="tel:0276248778" />
             </div>
         );
     }

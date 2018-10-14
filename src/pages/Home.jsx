d9b9a3864ac73402ff7d36649693b043ae4981c1
Voidoz <canubetrusted@gmail.com>
2018-10-15 11:24:32 +1300

    reordering image numbers in home page code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { _NavBar, _Header, _Post } from '../components';
 
-import Img1 from '../assets/images/logo/Unsplash_pics/Natural Lashes.jpg';
+import Img1 from '../assets/images/logo/Unsplash_pics/B & W Wedding Image.jpg';
 import Img2 from '../assets/images/logo/Unsplash_pics/Lights & Lashes.jpg';
-import Img3 from '../assets/images/logo/Unsplash_pics/B & W Wedding Image.jpg';
+import Img3 from '../assets/images/logo/Unsplash_pics/Natural Lashes.jpg';
 
 export default class Home extends React.Component {
     render() {
@@ -13,9 +13,9 @@ export default class Home extends React.Component {
                 <_NavBar />
                 <_Header />
                 <_Post
-                    header="For those about to rock..."
+                    header="For those about to rock some lashes..."
                     paragraph="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati."
-                    img={Img3}
+                    img={Img1}
                 />
                 <_Post 
                     header="We salute you!"
@@ -25,7 +25,7 @@ export default class Home extends React.Component {
                 <_Post 
                     header="This should change!"
                     paragraph="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati."
-                    img={Img1}
+                    img={Img3}
                 />
                 {/* eslint-enable */}
             </React.Fragment>
